32eafa234e081ce26342903d20cb226a1e81d219
scratchmario123 <scratchmario123@gmail.com>
2023-12-04 19:04:25 +0800

    Day 4

---
+++ src/adventofcode2023/day4/ScratchcardsPartOne.java
@@ -0,0 +1,24 @@
+package adventofcode2023.day4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScratchcardsPartOne {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("src/adventofcode2023/day4/TestData.txt"));
+            int sum = 0;
+            while (scanner.hasNext()) {
+                String s = scanner.nextLine();
+                List<Integer> winning = Arrays.stream(s.split(": ")[1].trim().split(" \\| ")[0].replaceAll(" {2}"," ").split(" ")).map(Integer::parseInt).toList();
+                sum += (int) Math.pow(2,winning.stream().filter(e -> Arrays.stream(s.split(": ")[1].split(" \\| ")[1].trim().replaceAll(" {2}"," ").split(" ")).map(Integer::parseInt).toList().contains(e)).toList().size()-1);
+            }
+            System.out.println(sum);
+        } catch (IOException e) {
+            System.out.println("No such file found");
+        }
+    }
+}
---
+++ src/adventofcode2023/day4/ScratchcardsPartTwo.java
@@ -0,0 +1,35 @@
+package adventofcode2023.day4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScratchcardsPartTwo {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("src/adventofcode2023/day4/TestData.txt"));
+            int sum, cards = 0;
+            List<Integer> copies = new ArrayList<>();
+            while (scanner.hasNext()) {
+                String s = scanner.nextLine();
+                cards++;
+                int card = Integer.parseInt(s.split(":")[0].split("Card")[1].trim());
+                if (copies.size() < card) copies.add(1);
+                else copies.set(card-1,copies.get(card-1)+1);
+                List<Integer> winning = Arrays.stream(s.split(": ")[1].trim().split(" \\| ")[0].replaceAll(" {2}"," ").split(" ")).map(Integer::parseInt).toList();
+                int wins = winning.stream().filter(e -> Arrays.stream(s.split(": ")[1].split(" \\| ")[1].trim().replaceAll(" {2}"," ").split(" ")).map(Integer::parseInt).toList().contains(e)).toList().size();
+                for (int i = card; i < card+wins; i++) {
+                    if (copies.size() <= i) copies.add(copies.get(card-1));
+                    else copies.set(i,copies.get(i)+copies.get(card-1));
+                }
+            }
+            sum = copies.subList(0,cards).stream().mapToInt(Integer::intValue).sum();
+            System.out.println(sum);
+        } catch (IOException e) {
+            System.out.println("No such file found");
+        }
+    }
+}
