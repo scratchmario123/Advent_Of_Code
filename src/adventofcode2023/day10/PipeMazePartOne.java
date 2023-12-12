1f04071cde5f8342ce79602f89ecd440a31d4523
scratchmario123 <scratchmario123@gmail.com>
2023-12-12 19:31:23 +0800

    Day 10, 11

---
+++ src/adventofcode2023/day10/PipeMazePartOne.java
@@ -0,0 +1,51 @@
+package adventofcode2023.day10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class PipeMazePartOne {
+    public static void main(String[] args) {
+        try {
+            List<String> sl = Files.readAllLines(Path.of("src/adventofcode2023/day10/TestData.txt"));
+
+            char cur = '+';
+            int L = sl.get(0).length(), S = sl.size(), x = 0, y = 0, pSpace = -1;//pSpace: 0=above,1=right,2=below,3=left
+            outer: for (int i = 0; i < S; i++) {
+                for (int j = 0; j < L; j++) {
+                    if (sl.get(i).charAt(j)=='S') {
+                        x=j;
+                        y=i;
+                        break outer;
+                    }
+                }
+            }
+            int step = 0;
+            while (cur != 'S') {
+                if ((cur == 'L' || cur == 'J' || cur == '|' || cur == '+') && pSpace != 0 && y!=0 && (getChar(sl,y-1,x) == '|' || getChar(sl,y-1,x) == 'F' || getChar(sl,y-1,x) == '7' || getChar(sl,y-1,x) == 'S')) {
+                    y=y-1;
+                    pSpace = 2;
+                } else if ((cur == '7' || cur == 'F' || cur == '|' || cur == '+') && pSpace != 2 && y!= S-1 && (getChar(sl,y+1,x) == '|' || getChar(sl,y+1,x) == 'L' || getChar(sl,y+1,x) == 'J' || getChar(sl,y+1,x) == 'S')) {
+                    y=y+1;
+                    pSpace = 0;
+                } else if ((cur == 'L' || cur == 'F' || cur == '-' || cur == '+') && pSpace != 1 && x!= L-1 && (getChar(sl,y,x+1) == '-' || getChar(sl,y,x+1) == '7' || getChar(sl,y,x+1) == 'J' || getChar(sl,y,x+1) == 'S')) {
+                    x=x+1;
+                    pSpace = 3;
+                } else {
+                    x=x-1;
+                    pSpace = 1;
+                }
+                cur = getChar(sl,y,x);
+                step++;
+            }
+            System.out.println(step/2);
+        } catch (IOException e) {
+            System.out.println("No such file found");
+        }
+    }
+
+    static char getChar(List<String> arr, int i, int j) {
+        return arr.get(i).charAt(j);
+    }
+}
---
+++ src/adventofcode2023/day11/CosmicExpansionPartTwo.java
@@ -0,0 +1,43 @@
+package adventofcode2023.day11;
+
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.List;
+
+public class CosmicExpansionPartTwo {
+    public static void main(String[] args) {
+        try {
+            List<String> sl = Files.readAllLines(Path.of("src/adventofcode2023/day11/TestData.txt"));
+            Map<Integer, Point> points = new HashMap<>();
+            Set<Integer> expandX = new HashSet<>(), notExpandY = new HashSet<>();
+            int cnt = 0, S = sl.size(), L = sl.get(0).length();
+            long sum = 0;
+            for (int i = 0; i < S; i++) {
+                String s = sl.get(i);
+                if (!s.contains("#")) expandX.add(i);
+                else for (int j = 0; j < L; j++) {
+                    if (s.charAt(j)=='#') {
+                        notExpandY.add(j);
+                        points.put(cnt,new Point(i,j));
+                        cnt++;
+                    }
+                }
+            }
+            for (int i = 0; i < points.size()-1; i++) {
+                for (int j = i+1; j < points.size(); j++) {
+                    Point o = points.get(i), d = points.get(j);
+                    int x = Math.abs(o.x-d.x), y = Math.abs(o.y-d.y);
+                    long a = expandX.stream().filter(e -> e > Math.min(o.x,d.x) && e < Math.max(d.x,o.x)).count();
+                    long b = y == 0 ? 0 : y+1 - notExpandY.stream().filter(e -> e >= Math.min(o.y,d.y) && e <= Math.max(d.y,o.y)).count();
+                    sum += (x+y+(a+b)*999999);
+                }
+            }
+            System.out.println(sum);
+        } catch (IOException e) {
+            System.out.println("No such file found");
+        }
+    }
+}
